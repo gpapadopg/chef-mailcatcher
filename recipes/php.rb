9a08c303a562b08ce984442a62c28789b5f81301
George Papadopoulos <g.papadopg@gmail.com>
2013-09-17 10:41:24 +0300

    Ubuntu fixes

--- recipes/php.rb
+++ recipes/php.rb
@@ -7,7 +7,7 @@
 
 # Publish PHP configuration
 template "#{node['php']['ext_conf_dir']}/mailcatcher.ini" do
-    source "mailcatcher.ini.erb"
+    source "php/mailcatcher.ini.erb"
     owner "root"
     group "root"
     mode "0644"
