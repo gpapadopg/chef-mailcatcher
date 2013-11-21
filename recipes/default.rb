5b71d6a9666e96e4bde57a407891c8283fd96026
George Papadopoulos <g.papadopg@gmail.com>
2013-11-21 15:31:28 +0200

    ...

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,16 +17,25 @@ end
 # Install MailCatcher
 gem_package "mailcatcher"
 
+# Publish PHP configuration
+template "/etc/init.d/mailcatcher" do
+    source "mailcatcher.service.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+end
+
 # Generate the command
-command = ["mailcatcher"]
-command << "--http-ip #{node['mailcatcher']['http-ip']}"
-command << "--http-port #{node['mailcatcher']['http-port']}"
-command << "--smtp-ip #{node['mailcatcher']['smtp-ip']}"
-command << "--smtp-port #{node['mailcatcher']['smtp-port']}"
-command = command.join(" ")
+#command = ["mailcatcher"]
+#command << "--http-ip #{node['mailcatcher']['http-ip']}"
+#command << "--http-port #{node['mailcatcher']['http-port']}"
+#command << "--smtp-ip #{node['mailcatcher']['smtp-ip']}"
+#command << "--smtp-port #{node['mailcatcher']['smtp-port']}"
+#command = command.join(" ")
 
 # Start MailCatcher
-bash "mailcatcher" do
-    not_if "ps ax | grep -E 'mailcatche[r]'"
-    code command
-end
+#bash "mailcatcher" do
+#    not_if "ps ax | grep -E 'mailcatche[r]'"
+#    code command
+#end
